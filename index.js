69369d91da659e870d132258e1b981b91e883e29
Inna <inna.rekesh@gmail.com>
2020-12-10 22:02:36 +0300

    feat: chunk array in groups

--- index.js
+++ index.js
@@ -88,3 +88,13 @@ function mutation(arr) {
 }
 
 mutation(["Mary", "Aarmy"])
+
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+  while(arr.length){
+    result.push(arr.splice(0, size))
+  }
+  return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
