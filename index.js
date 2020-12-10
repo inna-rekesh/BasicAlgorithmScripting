c257f67686d8dea1ae3d1e75076431ed4684ad07
Inna <inna.rekesh@gmail.com>
2020-12-10 21:03:52 +0300

    feat: franken splice

--- index.js
+++ index.js
@@ -50,3 +50,11 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+function frankenSplice(arr1, arr2, n) {
+  let res = arr2.slice();
+  res.splice(n, 0, ...arr1)
+  return res;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
