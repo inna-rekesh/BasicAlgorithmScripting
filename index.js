895de92b19df866eb4c5d0f1dd5f4b8f6ba2ab06
Inna <inna.rekesh@gmail.com>
2020-12-10 20:38:56 +0300

    feat: truncate string

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function repeatStringNumTimes(str, num) {
   let result = ''
-  for(let i=0; i<num; i++){
-    result+=str
+  for (let i = 0; i < num; i++) {
+    result += str
   }
   return result;
 }
@@ -9,7 +9,13 @@ function repeatStringNumTimes(str, num) {
 repeatStringNumTimes("abc", 3);
 
 function confirmEnding(str, target) {
-  return str.slice(-target.length)===target;
+  return str.slice(-target.length) === target;
 }
 
 confirmEnding("Bastian", "n");
+
+function truncateString(str, num) {
+  return str.length > num ? str.slice(0, num).concat('...') : str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
