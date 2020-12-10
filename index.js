8dbb8745d8678b90165f6b5d68633e86c2d9d47f
Inna <inna.rekesh@gmail.com>
2020-12-10 20:48:58 +0300

    feat: find element

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+function findElement(arr, func) {
+  let num = 0;
+  for(let i=0; i<arr.length; i++){
+    if(func(arr[i])){
+      num = arr[i]
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0);
